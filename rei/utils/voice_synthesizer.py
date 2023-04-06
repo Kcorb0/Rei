1f2c44b24d731d446c86eb0cbd2bf8970334d9e6
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2023-04-06 16:12:39 +0100

    Created voice profile, and text-to-speech functionality.

---
+++ rei/utils/voice_synthesizer.py
@@ -0,0 +1,48 @@
+import os
+import json
+from google.cloud import texttospeech as tts
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "rei/utils/configs/credentials.json"
+
+
+def text_to_speech(text: str):
+    configs = voice_configs()
+    synthesizer_input = tts.SynthesisInput(text=text)
+    voice = tts.VoiceSelectionParams(
+        language_code=configs["languageCode"], name=configs["voiceId"]
+    )
+
+    audio_config = tts.AudioConfig(
+        audio_encoding=tts.AudioEncoding.LINEAR16,
+        pitch=configs["pitch"],
+        speaking_rate=configs["speakingRate"],
+    )
+
+    client = tts.TextToSpeechClient()
+    response = client.synthesize_speech(
+        input=synthesizer_input, voice=voice, audio_config=audio_config
+    )
+
+    with open("output.wav", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.wav"')
+
+
+def voice_configs():
+    voice_configs_file = open("rei/utils/configs/main-voice.json")
+    voice_id = json.load(voice_configs_file)
+
+    configs = {
+        "pitch": voice_id["audioConfig"]["pitch"],
+        "speakingRate": voice_id["audioConfig"]["speakingRate"],
+        "languageCode": voice_id["voice"]["languageCode"],
+        "voiceId": voice_id["voice"]["name"],
+    }
+
+    return configs
+
+
+if __name__ == "__main__":
+    test_text = "Is this thing working, Pog I guess it is, L_?"
+    text_to_speech(test_text)
